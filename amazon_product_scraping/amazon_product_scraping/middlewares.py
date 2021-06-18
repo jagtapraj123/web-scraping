34ed67bf26a41a900f1c79011790525588653180
puevigreven <pralay.cs14@iitp.ac.in>
2021-06-18 17:45:04 +0530

    add https url, indentation and requirement.txt

--- amazon_product_scraping/amazon_product_scraping/middlewares.py
+++ amazon_product_scraping/amazon_product_scraping/middlewares.py
@@ -53,7 +53,7 @@ class AmazonProductScrapingSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class AmazonProductScrapingDownloaderMiddleware:
@@ -100,4 +100,4 @@ class AmazonProductScrapingDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
