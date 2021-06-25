28fa897aa8ebaf7b8a2005f754dd3eb2e38a2373
vershasaxena91 <78845302+vershasaxena91@users.noreply.github.com>
2021-06-25 14:36:00 +0530

    Adding scraping features and fixed indentation

---
+++ amazon_product_scraping/items.py
@@ -0,0 +1,30 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class AmazonProductScrapingItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    product_name = scrapy.Field()
+    product_brand = scrapy.Field()
+    product_sale_price = scrapy.Field()
+    product_offers = scrapy.Field()
+    product_original_price = scrapy.Field()
+    product_fullfilled = scrapy.Field()
+    product_rating = scrapy.Field()
+    product_total_reviews = scrapy.Field()
+    product_availability = scrapy.Field()
+    product_category = scrapy.Field()
+    product_icons = scrapy.Field()
+    product_best_seller_rank = scrapy.Field()
+    product_details = scrapy.Field()
+    product_asin = scrapy.Field()
+    product_important_information = scrapy.Field()
+    product_description = scrapy.Field()
+    product_bought_together = scrapy.Field()
+    product_subscription_discount = scrapy.Field()
+    product_variations = scrapy.Field()
