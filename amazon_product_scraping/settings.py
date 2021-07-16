885b1c0d0795c985d3654d79b9b33ff956f9578b
vershasaxena91 <78845302+vershasaxena91@users.noreply.github.com>
2021-07-16 16:24:35 +0530

    change user-agent

--- amazon_product_scraping/settings.py
+++ amazon_product_scraping/settings.py
@@ -61,20 +61,21 @@ DOWNLOAD_DELAY = 5  # 3
 # DOWNLOADER_MIDDLEWARES = {
 #    'amazon_product_scraping.middlewares.AmazonProductScrapingDownloaderMiddleware': 543,
 # }
-DOWNLOADER_MIDDLEWARES = {
-    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
-    "scrapy.downloadermiddlewares.retry.RetryMiddleware": None,
-    "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
-    "scrapy_fake_useragent.middleware.RetryUserAgentMiddleware": 401,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+#     "scrapy.downloadermiddlewares.retry.RetryMiddleware": None,
+#     "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
+#     "scrapy_fake_useragent.middleware.RetryUserAgentMiddleware": 401,
+# }
 
-FAKEUSERAGENT_PROVIDERS = [
-    "scrapy_fake_useragent.providers.FakeUserAgentProvider",  # this is the first provider we'll try
-    "scrapy_fake_useragent.providers.FakerProvider",  # if FakeUserAgentProvider fails, we'll use faker to generate a user-agent string for us
-    "scrapy_fake_useragent.providers.FixedUserAgentProvider",  # fall back to USER_AGENT value
-]
+# FAKEUSERAGENT_PROVIDERS = [
+#     "scrapy_fake_useragent.providers.FakeUserAgentProvider",  # this is the first provider we'll try
+#     "scrapy_fake_useragent.providers.FakerProvider",  # if FakeUserAgentProvider fails, we'll use faker to generate a user-agent string for us
+#     "scrapy_fake_useragent.providers.FixedUserAgentProvider",  # fall back to USER_AGENT value
+# ]
 
-USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64)  AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+# USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64)  AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+USER_AGENT = "Mozilla/5.0"
 
 
 SPIDERMON_ENABLED = True
