c5b54a228c83d992050c311a44bc5e6e342c6f6a
vershasaxena91 <78845302+vershasaxena91@users.noreply.github.com>
2021-07-16 17:37:09 +0530

    update demo p

--- notebooks/demo_file.py
+++ notebooks/demo_file.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 client = MongoClient()
 DB = client["amazon_product_data"]
-collection = db["product_data"]
+collection = DB["product_data"]
 url = "http://www.amazon.in"
 HEADERS = ({'User-Agent':
                         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'})
