d9d486aedc29c9fd22ad5c4130786bedf1bfd8ff
vershasaxena91 <78845302+vershasaxena91@users.noreply.github.com>
2021-07-16 18:38:05 +0530

    update config

--- notebooks/demo_file.py
+++ notebooks/demo_file.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 client = MongoClient()
-DB = client["amazon_product_data"]
-collection = DB["product_data"]
+db = client["amazon_product_data"]
+collection = db["product_data"]
 url = "http://www.amazon.in"
 HEADERS = ({'User-Agent':
                         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'})
@@ -13,5 +13,5 @@ soup = BeautifulSoup(webpage.content, "html.parser")
 
 for element in soup.find_all(['a','link']):
     link = element.get('href')
-    db.collection.insertOne({'link':link})
+    collection.insert_one({'link':link})
     print(link)
