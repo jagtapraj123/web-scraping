52721e5914575b770c75971f4b4246f45fe6497c
vershasaxena91 <78845302+vershasaxena91@users.noreply.github.com>
2021-07-19 16:11:11 +0530

    change database name

--- amazon_product_scraping/settings.py
+++ amazon_product_scraping/settings.py
@@ -17,7 +17,7 @@ ITEM_PIPELINES = {
 }
 
 MONGO_URI = "mongodb://localhost:27017"
-MONGO_DATABASE = "amazon_product_data_scraping"
+MONGO_DATABASE = "amazon_product_data"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
