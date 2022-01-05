bfdd62326f425c2cffe416493e68429ae4f7f671
jagtapraj123 <jagtapraj123@gmail.com>
2022-01-05 22:34:50 +0530

    Modified Telegram Notifier for handling long messages.

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -5,4 +5,5 @@ class TelegramNotifier:
         self.client = Updater("2126958170:AAEj2G0a8j33aaPSgVydkqEQGlY4TTg5lHU")
 
     def send_message(self, msg):
-        self.client.bot.send_message("-736146093", msg)
+        for i in range(1+len(msg)//4096):
+            self.client.bot.send_message("-736146093", msg[i*4096:(i+1)*4096])
